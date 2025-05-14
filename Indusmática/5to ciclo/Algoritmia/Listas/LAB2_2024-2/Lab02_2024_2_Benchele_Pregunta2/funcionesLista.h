5522b1f57e11fa9293d76ed56d180ecd9c0b1ad5
Benchele <mindomocomputo@gmail.com>
2025-05-13 19:08:55 -0500

    Add files via upload

---
+++ Indusmática/5to ciclo/Algoritmia/Listas/LAB2_2024-2/Lab02_2024_2_Benchele_Pregunta2/funcionesLista.h
@@ -0,0 +1,37 @@
+/* 
+ * File:   funcionesLista.h
+ * Author: ANA RONCAL
+ * Created on 11 de abril de 2025, 2:45 PM
+ */
+
+#ifndef FUNCIONESLISTA_H
+#define FUNCIONESLISTA_H
+
+#include "Pedido.h"
+
+void construir(struct Lista &);
+
+//void insertarAlInicioArchivo(const char *, struct Lista &);
+
+//void insertarAlInicio(struct Lista &, struct Pedido elemento);
+void insertarAlFinal(struct Lista &, struct Pedido elemento);
+//void insertarEnOrden(struct Lista &, int);
+
+struct Nodo * crearNodo(const struct Pedido & elemento, struct Nodo *);
+struct Nodo * obtenerUltimoNodo(const struct Lista &);
+//struct Nodo * obtenerNodoAnterior(const struct Lista &, int);
+//struct Nodo * seEncuentra(const struct Lista &, int);
+//Pedido retornaCabeza(const struct Lista & lista);
+
+bool esListaVacia(const struct Lista &);
+int longitud(const struct Lista &);
+
+//void eliminaNodo(struct Lista &, int);
+//void eliminaCabeza(struct Lista &);
+//void eliminaCola(struct Lista);
+
+void destruir(struct Lista &);
+void imprime(const struct Lista &);
+
+#endif /* FUNCIONESLISTA_H */
+
