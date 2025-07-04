904951072296a95865a5153e69af3b4653bb6747
ManduVK <kalefmandujano.av@gmail.com>
2025-07-03 21:51:48 -0500

    Add files via upload

---
+++ Indusmática/5to ciclo/Algoritmia/Arboles/P2_Lab04_2024_2 (ABB)/build/Debug/MinGW-Windows/main.o.d
@@ -0,0 +1,23 @@
+build/Debug/MinGW-Windows/main.o: main.cpp ArbolBB.h ArbolB.h NodoArbol.h \
+ funcionesABB.h funcionesAB.h funcionesPila.h Elemento.h Pila.h Lista.h \
+ Nodo.h
+
+ArbolBB.h:
+
+ArbolB.h:
+
+NodoArbol.h:
+
+funcionesABB.h:
+
+funcionesAB.h:
+
+funcionesPila.h:
+
+Elemento.h:
+
+Pila.h:
+
+Lista.h:
+
+Nodo.h:
