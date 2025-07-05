e518e51ad75e4b47431b2fefaec3d4ae60640728
JosetHuayta <josetsamuelhy25@gmail.com>
2025-07-04 23:00:20 -0500

    SkyNerd pero con arboles

---
+++ Indusmática/5to ciclo/Algoritmia/Arboles/ALG_ LAB5_23-1-yoshi/build/Debug/MinGW-Windows/main.o.d
@@ -0,0 +1,25 @@
+build/Debug/MinGW-Windows/main.o: main.cpp ArbolB.h NodoArbol.h \
+ Elemento.h funcionesAB.h Cola.h Lista.h Nodo.h funcionesCola.h Pila.h \
+ funcionesPila.h funcionesABB.h
+
+ArbolB.h:
+
+NodoArbol.h:
+
+Elemento.h:
+
+funcionesAB.h:
+
+Cola.h:
+
+Lista.h:
+
+Nodo.h:
+
+funcionesCola.h:
+
+Pila.h:
+
+funcionesPila.h:
+
+funcionesABB.h:
