d7d446982f355c99e3fc32977414f2d1a3e20d6d
JosetHuayta <josetsamuelhy25@gmail.com>
2025-07-08 20:07:53 -0500

    Completo (chiste?)

---
+++ Indusmática/5to ciclo/Algoritmia/EX2/EX2_24-2-yoshi/Pregunta1.a/Elemento.h
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   Elemento.h
+ * Author: Usuario
+ *
+ * Created on 8 de julio de 2025, 10:46
+ */
+
+#ifndef ELEMENTO_H
+#define ELEMENTO_H
+
+struct Elemento{
+    int lote;
+    int cant;
+};
+
+#endif /* ELEMENTO_H */
+
