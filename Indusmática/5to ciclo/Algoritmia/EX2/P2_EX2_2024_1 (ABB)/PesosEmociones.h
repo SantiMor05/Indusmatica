0bea9387ba71dcbb3d14e23c8b8f6860a067a7ac
ManduVK <kalefmandujano.av@gmail.com>
2025-07-09 23:00:25 -0500

    Add files via upload

---
+++ Indusmática/5to ciclo/Algoritmia/EX2/P2_EX2_2024_1 (ABB)/PesosEmociones.h
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   PesosEmociones.h
+ * Author: GINITA
+ *
+ * Created on 6 de julio de 2025, 07:02 PM
+ */
+
+#ifndef PESOSEMOCIONES_H
+#define PESOSEMOCIONES_H
+
+struct PesosEmociones{
+    int pesos[3];
+};
+
+
+#endif /* PESOSEMOCIONES_H */
+
