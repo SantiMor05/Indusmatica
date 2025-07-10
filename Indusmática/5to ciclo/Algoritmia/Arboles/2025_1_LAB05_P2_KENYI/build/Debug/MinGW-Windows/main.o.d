99f69cf57ad5fe8e75580dc32b9ea268dd2f1a60
Kenyi <164128877+KenyiCh@users.noreply.github.com>
2025-07-09 22:38:07 -0500

    Add files via upload

---
+++ Indusmática/5to ciclo/Algoritmia/Arboles/2025_1_LAB05_P2_KENYI/build/Debug/MinGW-Windows/main.o.d
@@ -0,0 +1,27 @@
+build/Debug/MinGW-Windows/main.o: main.cpp ArbolBB.h ArbolB.h NodoArbol.h \
+ Elemento.h Cola.h Lista.h Nodo.h funcionesABB.h funcionesAB.h \
+ funcionesCola.h funcionesPila.h Pila.h
+
+ArbolBB.h:
+
+ArbolB.h:
+
+NodoArbol.h:
+
+Elemento.h:
+
+Cola.h:
+
+Lista.h:
+
+Nodo.h:
+
+funcionesABB.h:
+
+funcionesAB.h:
+
+funcionesCola.h:
+
+funcionesPila.h:
+
+Pila.h:
