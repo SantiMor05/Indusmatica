97cd524987b64525fbc0ed57f13913a240a2977c
Santiago Moreno <148176264+SantiMor05@users.noreply.github.com>
2025-09-05 22:31:13 -0500

    Sudoku solver project
    Added CMake configuration, main.cpp with a backtracking Sudoku solver implementation, and IDE configuration files for project setup. This sets up the base for further development and testing of the Sudoku solver.

---
+++ Indusmática/6to ciclo/AlgoritmosAvanzados/LAB1/Sudoku-SantiMor/main.cpp
@@ -0,0 +1,107 @@
+#include <iomanip>
+#include <iostream>
+#define N 9
+#define NUMEROS 9
+
+using namespace std;
+
+int matriz[N][N] =
+    {
+    {0,0,3,0,7,6,0,0,2},
+    {0,0,0,0,0,0,0,0,0},
+    {0,4,0,5,0,1,0,9,0},
+    {6,0,1,8,0,0,7,0,0},
+    {0,0,0,0,0,0,0,0,0},
+    {0,5,0,0,0,0,4,2,3},
+    {0,0,2,7,0,0,0,0,0},
+    {0,0,0,6,3,9,0,0,7},
+    {0,0,0,0,8,0,0,4,0},
+};
+
+int cont = 0;
+
+int verificar(int num,int fil,int col) {
+
+    //Revisamos la fila (en horizontal)
+    for (int i = 0; i < N; i++) {
+        //if (i == col) continue;
+        if (num == matriz[fil][i]) return 0;
+    }
+    //Revisamos la columna (en vertical)
+    for (int i = 0; i < N; i++) {
+        //if (i == fil)continue;
+        if (num == matriz[i][col]) return 0;
+    }
+
+    //Revisamos el bloque
+    int fil1 = fil / 3 * 3;
+    int col1 = col / 3 * 3;
+
+    for (int i = fil1; i < fil1 + 3; i++) {
+        for (int j = col1; j < col1 + 3; j++) {
+            if (i == fil && j == col) continue;
+            if (matriz[i][j] == num) return 0;
+        }
+    }
+
+    return 1;
+}
+
+int posibilidades[9]{1,2,3,4,5,6,7,8,9};
+
+void imprimirMatriz() {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << setw(2) << matriz[i][j];
+            if ((j + 1) % 3 == 0 && j != N - 1)
+                cout << "|";
+        }
+
+        if ((i + 1) % 3 == 0 && i != N - 1) {
+            cout << endl;
+            cout << "---------------------------";
+            cout << endl;
+        }
+        else {
+            cout << endl;
+        }
+
+
+    }
+}
+
+void incrementarValor(int &nfil, int &ncol, int fil, int col) {
+    if (col + 1 >= N) {
+        ncol=0;
+        nfil  = fil + 1;
+    }
+    else ncol = col + 1;
+}
+
+int resolverSudoku(int fil, int col) {
+    if (fil == N) {
+        imprimirMatriz();
+        return 1;
+    }
+    int nfil = fil, ncol = col;
+    if (matriz[fil][col] == 0) {
+        for (int i = 0; i < NUMEROS; i++) {
+            if (verificar(posibilidades[i], fil, col)) {
+                matriz[fil][col] = posibilidades[i];
+                incrementarValor(nfil,ncol, fil,col);
+                if (resolverSudoku(nfil, ncol)) return 1;
+                matriz[fil][col] = 0;
+            }
+        }
+    }
+    else {
+        incrementarValor(nfil,ncol, fil, col);
+        return resolverSudoku(nfil, ncol);
+    }
+    return 0;
+}
+
+int main() {
+    resolverSudoku(0, 0);
+    return 0;
+}
