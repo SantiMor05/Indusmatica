f994bb5069288850d50fcd1b81864456b84897dd
Edward Delgadillo Agurto <a20230636@pucp.edu.pe>
2025-09-19 18:35:17 -0500

    correcion de subSecuenciaMasLarga

--- Indusmática/6to ciclo/AlgoritmosAvanzados/LAB2/subSecuenciaLarga-Edward/main.cpp
+++ Indusmática/6to ciclo/AlgoritmosAvanzados/LAB2/subSecuenciaLarga-Edward/main.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 using namespace std;
-#define N 8
-
 
 int hallarSubsecuenciaCreci(int*arr,int n) {
 
-    int solus[N+1]{},mayor=0;
+    int solus[n]{},mayor=INT_MIN;
     for(int i=1;i<n;i++) {
         if (arr[i]>mayor) {
             solus[i]=solus[i-1]+1;
             mayor=arr[i];
         }
-        else solus[i]=solus[i-1];
+        else {
+            int cant=0,ant=INT_MIN;
+            for(int j=1;j<i;j++) {
+                if (arr[j]<arr[i] and arr[j]>ant) {
+                    cant++;
+                    ant=arr[j];
+                }
+            }
+            if (cant>solus[i-1]) {
+                solus[i]=cant;
+                mayor=cant;
+            }
+            else solus[i]=solus[i-1];
+        }
+        cout<<solus[i]<<" ";
     }
+    cout<<endl;
     return solus[n-1];
 }
 
 int main() {
-    int arr[N+1]{0,10,22,9,33,21,50,41,60};
+    int arr[]{0,10,22,9,33,21,50,41,60};
+    //int arr[]{0,-7,10,9,2,3,8,8,1};
     int n=sizeof(arr)/sizeof(arr[0]);
 
     cout<<"Cantidad de subsecuencia mas larga: "<<hallarSubsecuenciaCreci(arr,n)<<endl;
